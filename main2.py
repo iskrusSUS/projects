8f8ccfd792d937a42bcb4b4bec3bfe6ca79143f9
iskrusSUS <118674244+iskrusSUS@users.noreply.github.com>
2023-01-24 17:04:30 +0100

    Add files via upload
    yes

---
+++ main2.py
@@ -0,0 +1,40 @@
+class Car:
+    wheels = 4
+    speed = 160
+    color = 'green'
+    power = 85
+    Seats = 4
+
+
+print(Car.wheels)
+print(Car.speed)
+print(Car.color)
+print(Car.power)
+print(Car.speed)
+
+class monkey:
+    braincells = 400
+    speed = 10
+    type = 'normal monkey'
+    power = 20
+    havekids = 'yes'
+
+
+print(monkey.braincells)
+print(monkey.speed)
+print(monkey.type)
+print(monkey.power)
+print(monkey.havekids)
+
+class Juice:
+    sugar = 20
+    salt = 10
+    type = 'orange juice'
+    nutriens = 20
+    kidsjuice = 'yes'
+
+print(Juice.sugar)
+print(Juice.salt)
+print(Juice.type)
+print(Juice.nutriens)
+print(Juice.kidsjuice)
